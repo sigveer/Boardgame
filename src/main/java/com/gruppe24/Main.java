1c9cd2560b428693eeeec21f6a885c6946f3fb86
sigveer <sigveer@stud.ntnu.no>
2025-02-19 14:46:41 +0100

    Refactor Main class to integrate JavaFX application structure

--- src/main/java/com/gruppe24/Main.java
+++ src/main/java/com/gruppe24/Main.java
@@ -1,14 +1,29 @@
 package com.gruppe24;
 
-import com.gruppe24.BoardGames.Menu;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
+public class Main extends Application {
 
-public class Main{
+  @Override
+  public void start(Stage stage) {
+    String javaVersion = System.getProperty("java.version");
+    String javafxVersion = System.getProperty("javafx.version");
+    Label l = new Label(
+        "Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+    Scene scene = new Scene(new StackPane(l), 640, 480);
+    stage.setScene(scene);
+    stage.show();
+  }
+
   public static void main(String[] args) {
-    Menu menu = new Menu();
-    menu.start();
+    launch();
   }
 }
+
 
 
 
