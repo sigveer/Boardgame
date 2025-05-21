4f551613388989b3f829cf615c21af4de6e91cc8
Ingve Værnes <ingve09@gmail.com>
2025-05-21 23:18:08 +0200

    removed monopoly controller and player

--- src/main/java/com/gruppe24/boardgames/monopolylite/MonopolyApp.java
+++ src/main/java/com/gruppe24/boardgames/monopolylite/MonopolyApp.java
@@ -1,7 +1,6 @@
 package com.gruppe24.boardgames.monopolylite;
 
 import com.gruppe24.boardgames.monopolylite.view.View;
-
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -9,12 +8,6 @@ public class MonopolyApp extends Application {
 
   @Override
   public void start(Stage primaryStage) {
-//    Model model = new Model();
-//    Controller controller = new Controller(model);
-//    View view = new View(primaryStage, model, controller);
-//
-//    controller.setView(view);
-
     View view = new View(primaryStage);
 
     view.initializeView();
